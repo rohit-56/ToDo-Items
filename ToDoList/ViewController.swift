111ea3e03016e382ecc24bc50677d0c788698b76
rohit-56 <rohitmou25@gmail.com>
2023-01-05 12:37:48 +0530

    add timer in main VC

--- ToDoList/ViewController.swift
+++ ToDoList/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(switchToDoVC), userInfo: nil, repeats: false)
+        
     }
-
-
+    
+    @objc func switchToDoVC(){
+        let storyboard = UIStoryboard(name: "ToDo", bundle: nil)
+        let vc = storyboard.instantiateViewController(withIdentifier: "ToDoViewController") as UIViewController
+        vc.modalPresentationStyle = .fullScreen
+        vc.modalTransitionStyle = .coverVertical
+        self.present(vc, animated: true)
+    }
 }
+
+
+    
 
